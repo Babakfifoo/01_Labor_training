1341bcdab233f1ad7bdb0cae83825185dfc59ee8
babakfifoo <b.firoozi.f@gmail.com>
2024-03-03 20:26:45 +0200

    queries dir name changed

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 import json
 STATFI_API_URL: str = "https://pxdata.stat.fi:443/PxWeb/api/v1/en/StatFin/"
 
-with open('../query/startup_total.json') as f:
+with open('../queries/startup_total.json') as f:
 	STARTUP_QUERY = json.loads(f.read())
