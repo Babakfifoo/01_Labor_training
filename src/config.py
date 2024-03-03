79b74490a971c836e28c51bd63e6a78e7e83e571
babakfifoo <b.firoozi.f@gmail.com>
2024-03-03 20:18:39 +0200

    refactored the repository to the ipynb from quarto

--- src/config.py
+++ src/config.py
@@ -1,55 +1,5 @@
+import json
 STATFI_API_URL: str = "https://pxdata.stat.fi:443/PxWeb/api/v1/en/StatFin/"
 
-STARTUP_QUERY = {
-	"query": [
-		{
-			"code": "Alue",
-			"selection": {
-				"filter": "item",
-				"values": [
-					"SSS"
-				]
-			}
-		},
-		{
-			"code": "Sukupuoli",
-			"selection": {
-				"filter": "item",
-				"values": [
-					"SSS"
-				]
-			}
-		},
-		{
-			"code": "Ikäryhmitys",
-			"selection": {
-				"filter": "item",
-				"values": [
-					"SSS"
-				]
-			}
-		},
-		{
-			"code": "Työllistämisen laji",
-			"selection": {
-				"filter": "item",
-				"values": [
-					"61",
-					"62"
-				]
-			}
-		},
-		{
-			"code": "Tiedot",
-			"selection": {
-				"filter": "item",
-				"values": [
-					"TYOLLISTETTYLOP"
-				]
-			}
-		}
-	],
-	"response": {
-		"format": "json-stat2"
-	}
-}
+with open('../query/startup_total.json') as f:
+	STARTUP_QUERY = json.loads(f.read())
